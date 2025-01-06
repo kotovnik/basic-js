687cf0d0102f506acf8a9f3d4fb86baeffa14a8e
kotovnik <kotovnik2024tbilisi@gmail.com>
2025-01-06 10:10:14 +0400

    feat: add solution for dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -6,20 +6,30 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const decayRate = 15;
+  const halfLife = 5730;
+  if (!(typeof sampleActivity === 'string') || isNaN(parseFloat(sampleActivity))||
+  parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) > decayRate) {
+    return false;
+  }
+  const activ = parseFloat(sampleActivity);
+  const log2 = Math.log(2);
+  const k = log2 / halfLife;
+  const time = Math.log(decayRate / activ) / k;
+  const result = Math.ceil(time);
+  return result;
 }
 
 module.exports = {
